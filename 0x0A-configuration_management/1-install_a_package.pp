23adb2d62c8f6d8a43401bfe22daa277fdecc40c
root <root@dennismutitu.localdomain>
2024-02-23 13:15:48 +0300

    i will install flask once i install pip3

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+#Install a package flask version 2.1.0  from pip3
+
+package{'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
+package{'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
+}
